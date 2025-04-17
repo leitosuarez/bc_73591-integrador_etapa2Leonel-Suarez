2ab49ac9ab47a3b52a572f07d64c15e1df909ce5
leitosuarez <leoneljunio123@gmail.com>
2025-04-17 05:00:58 -0300

    agrego la funcionalidad para incrementar lacantidad de productos en el carrito, mas la actualizacion de su precio.

--- src/components/ItemCarrito.jsx
+++ src/components/ItemCarrito.jsx
@@ -1,28 +1,75 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import CarritoContext from '../contexts/CarritoContext'
+import './ItemCarrito.scss'
+import convertCurrency from '../helpers/convert-currency'
+import { peticionesHttp } from '../helpers/peticiones-http'
+const ItemCarrito = ({productoCarrito}) => {
 
-const ItemCarrito = ({producto}) => {
 
-    const { eliminarProductoCarrito } = useContext(CarritoContext)
+    const { eliminarProductoCarrito, agregarProductoCarritoContext } = useContext(CarritoContext)
+    const [productsCount, setProductsCount] = useState(productoCarrito.cantidad)
+
     
-        const handleEliminar = (id) => {
-            console.log('Eliminando el producto...', id)
-            eliminarProductoCarrito(id)
-        }
+    const incrementCounter = (productoCarrito)=> {
+        //debugger
+        agregarProductoCarritoContext(productoCarrito)
+        setProductsCount(productsCount + 1)
+
+        
+    }
+    
+    const handleEliminar = (id) => {
+        console.log('Eliminando el productoCarrito...', id)
+        eliminarProductoCarrito(id)
+    }
 
   return (
-    <tr>
-        <td>
-            <img src={producto.foto} alt={producto.nombre} width="50px" />
-        </td>
-        <td>{producto.nombre}</td>
-        <td>{producto.cantidad}</td>
-        <td>{producto.precio}</td>
-        <td>
-            <button onClick={() => handleEliminar(producto.id)}>Eliminar</button>
-        </td>
-    </tr>
+   <div className='item-carrito'>
+    <div className='item-carrito__imagen'>
+        <img src={productoCarrito.foto} alt={productoCarrito.nombre} />
+    </div>
+    <div className='item-carrito__nombre-acciones'>
+        <div className='item-carrito__nombre-acciones__nombre'>
+            <h2>{productoCarrito.nombre}</h2>
+        </div>
+        <div className='item-carrito__nombre-acciones__acciones'>
+            <button
+            className='delete-button'
+            onClick={handleEliminar}
+            >
+                <i class="fa-regular fa-trash-can"></i>
+            </button>
+            <button
+            className='buy-button'
+            >
+                
+                Comprar ahora
+            </button>
+        </div>
+    </div>
+    <div className='item-carrito__cantidad-precio'>
+        <div className='item-carrito__cantidad-precio__cantidad'>
+            {'Cantidad:'+ ' '+ productsCount}
+            <button onClick={()=> incrementCounter(productoCarrito)}>+</button>
+        </div > 
+        <p>
+            {'$'+convertCurrency(productoCarrito.precio * productsCount)}
+        </p>
+    </div>
+   </div>
   )
 }
 
 export default ItemCarrito
+
+ {/* <tr>
+        <td>
+            <img src={productoCarrito.foto} alt={productoCarrito.nombre} width="50px" />
+        </td>
+        <td>{productoCarrito.nombre}</td>
+        <td>{productoCarrito.cantidad}</td>
+        <td>{'$'+convertCurrency(productoCarrito.precio * productoCarrito.cantidad)}</td>
+        <td>
+            <button onClick={() => handleEliminar(productoCarrito.id)}>Eliminar</button>
+        </td>
+    </tr> */}
